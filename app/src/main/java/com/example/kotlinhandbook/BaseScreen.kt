fdaadd93238ccc9006585c77195d2f745245e85a
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-11 23:30:52 +0300

    added interfaces as a base structure

---
+++ app/src/main/java/com/example/kotlinhandbook/BaseScreen.kt
@@ -0,0 +1,6 @@
+package com.example.kotlinhandbook
+
+import java.io.Serializable
+
+interface BaseScreen : Serializable {
+}
---
+++ app/src/main/java/com/example/kotlinhandbook/Repository.kt
@@ -0,0 +1,4 @@
+package com.example.kotlinhandbook
+
+interface Repository {
+}
