ccf938987d11b39f516765c44a89f6b5e393caef
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-11 23:50:34 +0300

    removed to MODEL

--- app/src/main/java/com/example/kotlinhandbook/Repository.kt
+++
@@ -1,4 +0,0 @@
-package com.example.kotlinhandbook
-
-interface Repository {
-}
---
+++ app/src/main/java/com/example/kotlinhandbook/model/Repository.kt
@@ -0,0 +1,6 @@
+package com.example.kotlinhandbook.model
+
+/*This interface is a marker for all implemented repository classes*/
+
+interface Repository {
+}
