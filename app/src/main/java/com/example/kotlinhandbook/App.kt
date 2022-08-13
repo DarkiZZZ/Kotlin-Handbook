60ccd437faa989be6e7a56971895c08660315ec8
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-13 13:08:46 +0300

    filled App class and made it singleton. Added to repos to models

--- app/src/main/java/com/example/kotlinhandbook/App.kt
+++ app/src/main/java/com/example/kotlinhandbook/App.kt
@@ -1,4 +1,13 @@
 package com.example.kotlinhandbook
 
-class App {
+import android.app.Application
+import com.example.kotlinhandbook.model.maintopicdata.MainTopicRepository
+import com.example.kotlinhandbook.model.subtopicdata.SubTopicRepository
+
+class App : Application() {
+
+    val models = listOf<Any>(
+        MainTopicRepository(),
+        SubTopicRepository()
+    )
 }
