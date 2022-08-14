acfbbbad7ef7d56c589246d4f54d3c6b671fc3a9
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-14 12:34:47 +0300

    filled functions with arguments

--- app/src/main/java/com/example/kotlinhandbook/views/Navigator.kt
+++ app/src/main/java/com/example/kotlinhandbook/views/Navigator.kt
@@ -1,10 +1,12 @@
 package com.example.kotlinhandbook.views
 
+import com.example.kotlinhandbook.views.base.BaseScreen
+
 /*This interface shows actions, which responsible for navigation between fragments*/
 
 interface Navigator {
 
-    fun launch(/*todo: as argument - screen(fragment basically)*/)
+    fun launch(screen: BaseScreen)
 
-    fun goBack(/*todo: as argument - Result class, through this class i will transfer information between fragments*/)
+    fun goBack(result: Any? = null)
 }
