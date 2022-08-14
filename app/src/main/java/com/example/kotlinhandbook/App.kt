438f4057f13576b133c173feb8890c2ef96afcd0
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-14 11:38:55 +0300

    removed excess repository

--- app/src/main/java/com/example/kotlinhandbook/App.kt
+++ app/src/main/java/com/example/kotlinhandbook/App.kt
@@ -2,12 +2,10 @@ package com.example.kotlinhandbook
 
 import android.app.Application
 import com.example.kotlinhandbook.model.maintopicdata.TopicRepository
-import com.example.kotlinhandbook.model.subtopicdata.SubTopicRepository
 
 class App : Application() {
 
     val models = listOf<Any>(
-        TopicRepository(),
-        SubTopicRepository()
+        TopicRepository()
     )
 }
