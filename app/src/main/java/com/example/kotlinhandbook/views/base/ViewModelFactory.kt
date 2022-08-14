bc48975324e661bf9cd00b0789dd38f1f3e71a12
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-14 11:59:04 +0300

    filled viewModel factory

--- app/src/main/java/com/example/kotlinhandbook/views/base/ViewModelFactory.kt
+++ app/src/main/java/com/example/kotlinhandbook/views/base/ViewModelFactory.kt
@@ -1,4 +1,59 @@
 package com.example.kotlinhandbook.views.base
 
-class ViewModelFactory {
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.savedstate.SavedStateRegistryOwner
+import com.example.kotlinhandbook.App
+import com.example.kotlinhandbook.MainViewModel
+import java.lang.reflect.Constructor
+
+inline fun <reified VM : ViewModel> BaseFragment.screenViewModel() = viewModels<VM> {
+    val application = requireActivity().application as App
+    val screen = requireArguments().getSerializable(/*todo ARG_SCREEN*/) as BaseScreen
+
+
+    val provider = ViewModelProvider(requireActivity(),
+        ViewModelProvider.AndroidViewModelFactory(application)
+    )
+    val mainViewModel = provider[MainViewModel::class.java]
+
+
+    val dependencies = listOf(screen, mainViewModel) + application.models
+
+
+    ViewModelFactory(dependencies, this)
+}
+
+class ViewModelFactory(
+    private val dependencies: List<Any>,
+    owner: SavedStateRegistryOwner
+) : AbstractSavedStateViewModelFactory(owner, null) {
+
+    override fun <T : ViewModel?> create(
+        key: String,
+        modelClass: Class<T>,
+        handle: SavedStateHandle
+    ): T {
+        val constructors = modelClass.constructors
+        val constructor = constructors.maxByOrNull { it.typeParameters.size }!!
+
+        val dependenciesWithSavedState = dependencies + handle
+
+        val arguments = findDependencies(constructor, dependenciesWithSavedState)
+
+        return constructor.newInstance(*arguments.toTypedArray()) as T
+    }
+
+    private fun findDependencies(constructor: Constructor<*>, dependencies: List<Any>): List<Any> {
+        val args = mutableListOf<Any>()
+
+        constructor.parameterTypes.forEach { parameterClass ->
+            val dependency = dependencies.first { parameterClass.isAssignableFrom(it.javaClass) }
+            args.add(dependency)
+        }
+        return args
+    }
 }
