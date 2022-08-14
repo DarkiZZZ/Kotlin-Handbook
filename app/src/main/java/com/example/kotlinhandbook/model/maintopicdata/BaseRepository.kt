666fc3ab3c83b64496f193eabb6140e2cdb2d49d
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-14 11:37:33 +0300

    added interface for all repositories

---
+++ app/src/main/java/com/example/kotlinhandbook/model/maintopicdata/BaseRepository.kt
@@ -0,0 +1,19 @@
+package com.example.kotlinhandbook.model.maintopicdata
+
+import com.example.kotlinhandbook.model.Repository
+
+typealias MainListener = (MainTopic) -> Unit
+
+interface BaseRepository : Repository {
+
+    var currentTopic: MainTopic
+
+    fun getAvailableTopics(): List<MainTopic>
+
+    fun getById(id: Long): MainTopic
+
+    fun addListener(listener: MainListener)
+
+    fun removeListener(listener: MainListener)
+
+}
