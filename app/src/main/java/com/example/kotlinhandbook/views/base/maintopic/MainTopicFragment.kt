29072b4054b4a24a225b042fc8f09ab7c5c730ac
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-15 11:14:06 +0300

    moved it to fragment to get access to viewmodel from adapter

--- app/src/main/java/com/example/kotlinhandbook/views/base/maintopic/MainTopicFragment.kt
+++ app/src/main/java/com/example/kotlinhandbook/views/base/maintopic/MainTopicFragment.kt
@@ -1,4 +1,72 @@
 package com.example.kotlinhandbook.views.base.maintopic
 
-class MainTopicFragment {
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+import com.example.kotlinhandbook.views.base.BaseFragment
+import com.example.kotlinhandbook.views.base.BaseScreen
+import com.example.kotlinhandbook.views.base.BaseViewModel
+
+class MainTopicFragment: BaseFragment() {
+
+    override val viewModel: BaseViewModel
+        get() = TODO("Not yet implemented")
+
+    class Screen : BaseScreen
+
+    class ListViewAdapter: BaseExpandableListAdapter(){
+
+        override fun getGroupCount(): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun getChildrenCount(groupPosition: Int): Int {
+            TODO("Not yet implemented")
+        }
+
+        override fun getGroup(groupPosition: Int): Any {
+            TODO("Not yet implemented")
+        }
+
+        override fun getChild(groupPosition: Int, childPosition: Int): Any {
+            TODO("Not yet implemented")
+        }
+
+        override fun getGroupId(groupPosition: Int): Long {
+            TODO("Not yet implemented")
+        }
+
+        override fun getChildId(groupPosition: Int, childPosition: Int): Long {
+            TODO("Not yet implemented")
+        }
+
+        override fun hasStableIds(): Boolean {
+            TODO("Not yet implemented")
+        }
+
+        override fun getGroupView(
+            groupPosition: Int,
+            isExpanded: Boolean,
+            convertView: View?,
+            parent: ViewGroup?
+        ): View {
+            TODO("Not yet implemented")
+        }
+
+        override fun getChildView(
+            groupPosition: Int,
+            childPosition: Int,
+            isLastChild: Boolean,
+            convertView: View?,
+            parent: ViewGroup?
+        ): View {
+            TODO("Not yet implemented")
+        }
+
+        override fun isChildSelectable(groupPosition: Int, childPosition: Int): Boolean {
+            TODO("Not yet implemented")
+        }
+
+
+    }
 }
