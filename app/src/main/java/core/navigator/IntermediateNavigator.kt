51fffcca0bc9e07676caa48aa44b1a49bf9aed89
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-16 16:08:10 +0300

    activity navigator implementation

--- app/src/main/java/core/navigator/IntermediateNavigator.kt
+++ app/src/main/java/core/navigator/IntermediateNavigator.kt
@@ -1,4 +1,25 @@
 package core.navigator
 
-class IntermediateNavigator {
+import core.utils.ResourceActions
+import core.views.BaseScreen
+
+class IntermediateNavigator : Navigator {
+
+    private val targetNavigator = ResourceActions<Navigator>()
+
+    override fun launch(screen: BaseScreen) = targetNavigator{
+        it.launch(screen)
+    }
+
+    override fun goBack(result: Any?) = targetNavigator{
+        it.goBack(result)
+    }
+
+    fun setTarget(navigator: Navigator?){
+        targetNavigator.resource = navigator
+    }
+
+    fun clear(){
+        targetNavigator.clear()
+    }
 }
