f19e05bbe8fbd0935426f3c170094f3b04121d1d
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-16 22:44:50 +0300

    added factory for ActivityScopeViewModel.kt

--- app/src/main/java/core/utils/ViewModelCreator.kt
+++ app/src/main/java/core/utils/ViewModelCreator.kt
@@ -1,3 +1,24 @@
 package core.utils
 
-class ViewModelFactory
+import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+typealias ViewModelCreator = () -> ViewModel?
+
+@Suppress("UNCHECKED_CAST")
+class ViewModelFactory(
+    private val creator: ViewModelCreator
+) : ViewModelProvider.Factory{
+
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+
+        return creator() as T
+    }
+}
+
+inline fun <reified VM: ViewModel> ComponentActivity.viewModelCreator(noinline creator: ViewModelCreator):
+        Lazy<VM>{
+    return viewModels { ViewModelFactory(creator) }
+}
