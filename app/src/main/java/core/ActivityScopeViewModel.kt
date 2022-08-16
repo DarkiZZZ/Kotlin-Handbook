a5f4c40a84706e475342b1b7871b9f7b6878d21c
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-16 22:49:36 +0300

    added initial parameters

--- app/src/main/java/core/ActivityScopeViewModel.kt
+++ app/src/main/java/core/ActivityScopeViewModel.kt
@@ -1,4 +1,20 @@
 package core
 
-class ActivityScopeViewModel {
+import androidx.lifecycle.ViewModel
+import core.navigator.IntermediateNavigator
+import core.navigator.Navigator
+import core.uiactions.UiActions
+
+const val ARG_SCREEN = "ARG_SCREEN"
+
+class ActivityScopeViewModel(
+    val uiActions: UiActions,
+    val navigator: IntermediateNavigator
+): ViewModel(), Navigator by navigator, UiActions by uiActions{
+
+    override fun onCleared() {
+        super.onCleared()
+        navigator.clear()
+    }
+
 }
