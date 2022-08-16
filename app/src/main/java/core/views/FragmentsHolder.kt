d2782b4247b18b512e19158d89e33881ed376af0
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-16 22:41:12 +0300

    added functions

--- app/src/main/java/core/views/FragmentsHolder.kt
+++ app/src/main/java/core/views/FragmentsHolder.kt
@@ -1,4 +1,10 @@
 package core.views
 
+import core.ActivityScopeViewModel
+
 interface FragmentsHolder {
+
+    fun notifyScreenUpdates()
+
+    fun getActivityScopeViewModel(): ActivityScopeViewModel
 }
