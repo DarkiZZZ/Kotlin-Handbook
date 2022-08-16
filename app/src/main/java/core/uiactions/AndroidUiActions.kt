7b276c006024ffe7fd50e255b93d5b935bcdc11d
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-16 16:07:41 +0300

    UiActions implementation

--- app/src/main/java/core/uiactions/AndroidUiActions.kt
+++ app/src/main/java/core/uiactions/AndroidUiActions.kt
@@ -1,4 +1,16 @@
 package core.uiactions
 
-class AndroidUiActions {
+import android.content.Context
+import android.widget.Toast
+
+class AndroidUiActions(
+    private val appContext: Context): UiActions {
+
+    override fun toast(message: String) {
+        Toast.makeText(appContext, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun getString(messageRes: Int, vararg args: Any): String {
+        return appContext.getString(messageRes, *args)
+    }
 }
