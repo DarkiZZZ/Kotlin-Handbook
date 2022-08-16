4c73e0b5236d8a6ca41c1b19a921c3e97b434c24
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-16 22:42:36 +0300

    added init parameters

--- app/src/main/java/com/example/kotlinhandbook/views/base/maintopic/MainTopicViewModel.kt
+++ app/src/main/java/com/example/kotlinhandbook/views/base/maintopic/MainTopicViewModel.kt
@@ -1,4 +1,23 @@
 package com.example.kotlinhandbook.views.base.maintopic
 
-class MainTopicViewModel {
+import androidx.lifecycle.MutableLiveData
+import com.example.kotlinhandbook.model.data.BaseRepository
+import com.example.kotlinhandbook.model.data.MainTopic
+import core.navigator.Navigator
+import core.uiactions.UiActions
+import core.views.BaseViewModel
+
+class MainTopicViewModel(
+    private val navigator: Navigator,
+    private val uiActions: UiActions,
+    private val repository: BaseRepository
+): BaseViewModel(), ListViewAdapter.ItemListener {
+
+
+
+    override fun onSubTopicChosen(topic: MainTopic) {
+
+    }
+
+
 }
