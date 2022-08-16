9a90bc56e41d0336d577f5d3c14eaa130f7a0b6f
Mike Sokolov <mark_sokolov_2016@mail.ru>
2022-08-16 22:46:33 +0300

    implemented 2 interfaces

--- app/src/main/java/com/example/kotlinhandbook/views/base/subtopic/SubTopicFragment.kt
+++ app/src/main/java/com/example/kotlinhandbook/views/base/subtopic/SubTopicFragment.kt
@@ -1,4 +1,15 @@
 package com.example.kotlinhandbook.views.base.subtopic
 
-class SubTopicFragment {
+import core.views.HasScreenTitle
+import core.views.BaseFragment
+import core.views.BaseViewModel
+
+class SubTopicFragment: BaseFragment(), HasScreenTitle {
+
+    override fun getScreenTitle(): String? {
+        TODO("Not yet implemented")
+    }
+
+    override val viewModel: BaseViewModel
+        get() = TODO("Not yet implemented")
 }
